7ba6979a69ea3b39a0ab68d461226f73385f6302
TanUnU165 <meollo74@gmail.com>
2023-10-23 16:19:38 +0700

    Hieu

--- Source.cpp
+++ Source.cpp
@@ -2,7 +2,14 @@
 
 using namespace std;
 int tong(int a, int b);
-int hieu(int a, int b);
+int hieu(int a, int b){
+	int hieu=0;
+	cout<<"\nNhap a va b";
+	cin>>a>>b;
+	hieu = a-b;
+	cout<<"\nHieu cua a va b: "<<hieu;
+	return hieu
+}
 int tich(int a, int b);
 float thuong(int a, int b);
 int main()
