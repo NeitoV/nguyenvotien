be5085e2d1dfd2aee61976bfe7461be2b06dc8c3
TanUnU165 <meollo74@gmail.com>
2023-10-23 16:23:53 +0700

    Hieu

--- Source.cpp
+++ Source.cpp
@@ -14,7 +14,9 @@ int tich(int a, int b);
 float thuong(int a, int b);
 int main()
 {
+	int a,b;
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
+	cout<< hieu(10,5);
 	system("pause");
 	return 0;
 }
