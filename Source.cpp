bcce7f71d58ec446fb1cae7d5cbb0a2d49691933
NeitoV <votien31102002@gmail.com>
2023-10-23 16:35:07 +0700

    fix bug do trung bao

--- Source.cpp
+++ Source.cpp
@@ -5,23 +5,10 @@ int tong(int a, int b) {
 	return a + b;
 }
 int hieu(int a, int b);
-int tich(int a, int b) {
-	cout << "\nNhap a :" << endl;
-	cin >> a;
-	cout << "\nNhap b :" << endl;
-	cin >> b;
-	int tich = 0;
-tich = a*b;
-	cout << " \nTich cua a va b la :" << tich << endl;
-	
-}
-
-
+int tich(int a, int b);
 float thuong(int a, int b);
 int main()
 {
-int a,b;
-cout<< tich(6,9);
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
 	cout <<tong(3,2);
 	system("pause");
