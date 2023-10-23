03ee956e6eede4ec1146e19bb244025e8ac0098c
shadichero327 <gamerrpg000@gmail.com>
2023-10-23 16:28:47 +0700

    tich

--- Source.cpp
+++ Source.cpp
@@ -3,10 +3,23 @@
 using namespace std;
 int tong(int a, int b);
 int hieu(int a, int b);
-int tich(int a, int b);
+int tich(int a, int b) {
+	cout << "\nNhap a :" << endl;
+	cin >> a;
+	cout << "\nNhap b :" << endl;
+	cin >> b;
+	int tich = 0;
+tich = a*b;
+	cout << " \nTich cua a va b la :" << tich << endl;
+	
+}
+
+
 float thuong(int a, int b);
 int main()
 {
+int a,b;
+cout<< tich(6,9);
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
 	system("pause");
 	return 0;
